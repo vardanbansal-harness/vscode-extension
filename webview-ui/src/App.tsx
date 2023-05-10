7d528bf291de2591adf61cd4539f03b9ddf33624
Vardan Bansal <vardan.bansal@harness.io>
2023-05-10 11:51:34 -0700

    adding monaco editor

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,5 +1,6 @@
 import { vscode } from "./utilities/vscode";
 import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
+import MonacoEditor from 'react-monaco-editor';
 import "./App.css";
 
 function App() {
@@ -10,10 +11,21 @@ function App() {
     });
   }
 
+  const value = `version: 1\noptions:\n  repository:\n    disabled: true\nstages:\n  - name: build1\n    desc: sample ci build stage\n    type: ci\n    spec:\n      steps:\n        - name: Run echo 1\n          spec:\n            run: echo \"hi from the demo\"\n          type: script\n        - name: Run echo 2\n          type: plugin\n          spec:\n            uses: docker\n            with:\n              repo: harness/hello-world\n              connector: account.dockerhub\n        - name: new_step\n          type: script\n          spec:\n            run: echo hi\n        - name: one_more_Step\n          type: action\nname: bugbash pipeline - Clone\n`
+
   return (
     <main>
       <h1>This text is coming from a React application</h1>
       <VSCodeButton onClick={interact}>Interact with VS Code</VSCodeButton>
+      <div className="editor">
+      <MonacoEditor 
+        width="800"
+        height="600"
+        language="yaml"
+        value={value}
+        theme="vs"
+        />
+        </div>
     </main>
   );
 }
