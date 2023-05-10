75ba3a67247a06b65c749c9d21764faac317f6d4
Vardan Bansal <vardan.bansal@harness.io>
2023-05-10 12:56:33 -0700

    changes to panel styling

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -2,8 +2,16 @@ import { vscode } from "./utilities/vscode";
 import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
 import MonacoEditor from 'react-monaco-editor';
 import "./App.css";
+import { useState } from "react";
+import { VSCodeTextArea } from "@vscode/webview-ui-toolkit/react";
+
+const value = `version: 1\noptions:\n  repository:\n    disabled: true\nstages:\n  - name: build1\n    desc: sample ci build stage\n    type: ci\n    spec:\n      steps:\n        - name: Run echo 1\n          spec:\n            run: echo \"hi from the demo\"\n          type: script\n        - name: Run echo 2\n          type: plugin\n          spec:\n            uses: docker\n            with:\n              repo: harness/hello-world\n              connector: account.dockerhub\n        - name: new_step\n          type: script\n          spec:\n            run: echo hi\n        - name: one_more_Step\n          type: action\nname: bugbash pipeline - Clone\n`
+const updatedValue = `version: 1\noptions:\n  repository:\n    disabled: true\nstages:\n  - name: build1\n    desc: sample ci build stage\n    type: ci\n    spec:\n      steps:\n        - name: Run Step\n          spec:\n            run: echo \"added from panel\"\n          type: script\n        - name: Run echo 1\n          spec:\n            run: echo \"hi from the demo\"\n          type: script\n        - name: Run echo 2\n          type: plugin\n          spec:\n            uses: docker\n            with:\n              repo: harness/hello-world\n              connector: account.dockerhub\n        - name: new_step\n          type: script\n          spec:\n            run: echo hi\n        - name: one_more_Step\n          type: action\nname: bugbash pipeline - Clone\n`
 
 function App() {
+
+  const [shouldRenderPluginForm, setShouldRenderPluginForm] = useState<boolean>(false)
+  const [yaml, setYaml] = useState<string>(value)
   function interact() {
     vscode.postMessage({
       command: "sendMessage",
@@ -11,20 +19,28 @@ function App() {
     });
   }
 
-  const value = `version: 1\noptions:\n  repository:\n    disabled: true\nstages:\n  - name: build1\n    desc: sample ci build stage\n    type: ci\n    spec:\n      steps:\n        - name: Run echo 1\n          spec:\n            run: echo \"hi from the demo\"\n          type: script\n        - name: Run echo 2\n          type: plugin\n          spec:\n            uses: docker\n            with:\n              repo: harness/hello-world\n              connector: account.dockerhub\n        - name: new_step\n          type: script\n          spec:\n            run: echo hi\n        - name: one_more_Step\n          type: action\nname: bugbash pipeline - Clone\n`
-
   return (
     <main>
-      <h1>This text is coming from a React application</h1>
       <VSCodeButton onClick={interact}>Interact with VS Code</VSCodeButton>
       <div className="editor">
-      <MonacoEditor 
-        width="800"
-        height="600"
-        language="yaml"
-        value={value}
-        theme="vs"
+        <MonacoEditor 
+          width='70vw'
+          height={'calc(100vh - 250px)'}
+          language="yaml"
+          value={yaml}
+          theme="vs"
+          options={{minimap: {enabled: false}}}
         />
+        <div className="panel">
+          {shouldRenderPluginForm ? 
+          <>
+            <VSCodeTextArea style={{width: '100%'}}/>
+            <VSCodeButton onClick={() => setYaml(updatedValue)} className="addBtn">Add</VSCodeButton>
+          </> : 
+          <div>
+            <VSCodeButton onClick={() => setShouldRenderPluginForm(true)}>Add a plugin</VSCodeButton>
+          </div>}
+        </div>
         </div>
     </main>
   );
